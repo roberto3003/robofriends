0591375178f8d4c25800068261d77e2c6d0dd63d
roberto3003 <roberto3003@riseup.net>
2020-05-07 00:41:16 +0700

    Updated packages

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,12 @@ import React from 'react';
 
 const card = ({ name, email, id }) => {
     return (
-        <id className= 'tc bg-light-green dib br3 pd3 
-            ma2 grow bw2 shadow-5'>
-        < img className = 'grow bw24'
-              src = {`https://robohash.org/${id}size=150x150`
-}
-              alt = 'robot' />
+        <div className= 'tc bg-light-green dib br3 pd3 ma2 grow bw2 shadow-5'>
+        < img className = 'grow bw24' src = {`https://robohash.org/${id}size=150x150`}
+            alt = 'robot' />
                 <h2>{name}</h2>
                 <p>{email}</p>
-            </id>
+            </div>
     );
 }
 
